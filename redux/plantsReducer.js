8fcc03c66c131e164225d8f07b199755dce9369c
AlexandraHessen <xl_energy@mail.ru>
2021-12-10 22:19:51 +0300

    with comments

--- redux/plantsReducer.js
+++ redux/plantsReducer.js
@@ -1,9 +1,12 @@
-﻿import { PLANTS_LOADING, PLANTS_ERROR, PLANTS_SET } from './plantsAC';
+﻿// ---------------- САМА ФУНКЦИЯ REDUСER  ----------------//
+// REDUCER не делает запрос в Ajax, логика должна быть в компоненте
+import { PLANTS_LOADING, PLANTS_ERROR, PLANTS_SET } from './plantsAC'; //название action type
 
-const initState={
+
+const initState={ // начальный state
 
   status: 0, // 0 - ничего не началось, 1 - идёт загрузка, 2 - была ошибка, 3 - данные загружены
-  data: null,
+  data: null, //даные которые мы загрузили по сети
 
 }
 
@@ -29,11 +32,12 @@ function plantsReducer(state=initState,action) {
     case PLANTS_SET: {
       let newState={
         status:3,
-        data:action.plants,
+        data:action.plants, //загруженные данные
       };
       return newState;
     }
     
+//обязатнльно пишем на тот случай если пришел action который не относится к этому reducery
     default:
       return state;
   }
