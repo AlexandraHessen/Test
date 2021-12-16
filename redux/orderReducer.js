d71cb24b1ab145d54c4c88856548b5e6b6f9b6b3
AlexandraHessen <xl_energy@mail.ru>
2021-12-16 20:17:26 +0300

    orderReducer

--- redux/orderReducer.js
+++ redux/orderReducer.js
@@ -4,7 +4,7 @@ import { ADD_ORDER } from './orderAC'; //название action type
 
 
 const initState={ // начальный state
-    orders: []
+    orders: {}
     //КЛЮЧИ productId, objProductInfo
     // 1: {code: 1, name: 'Замиокулькас'}
 
@@ -18,16 +18,33 @@ function orderReducer(state=initState,action) {
 
     case ADD_ORDER: {
       console.log('action:',action);
+      console.log(action.customerPhone)
+      console.log(action.objOrderInfo)
       console.log('state до обработки редьюсером:',state);
       let newState={...state, //старый state
-        orders:[...state.orders, //заменить старые продукты на теже +
+        orders:{...state.orders, //заменить старые продукты на теже +
           // ПРИЛЕТИТ какой продукт (productid) изменить и насколько(addvalue)
-          action.objProductInfo //+
+          
+          [action.customerPhone]:action.objOrderInfo //+
           //[action.counterid] - В нужном продукте 
           // state.productsInBasket[action.productid] - его старое значение 
           // action.addvalue - изменить на прилетевшее
-        ]
+        }
       };
+      console.log('state после обработки редьюсером:',newState);
+      return newState;
+    
+      
+      // let newState={...state, //старый state
+      //   orders=[ ...state.orders,//заменить старые заказы на теже +
+      //   // orders:[...state.orders, //заменить старые продукты на теже +
+      //     // ПРИЛЕТИТ какой продукт (productid) изменить и насколько(addvalue)
+      //     state.orders[2].push(action.objProductInfo) //+
+      //     //[action.counterid] - В нужном продукте 
+      //     // state.productsInBasket[action.productid] - его старое значение 
+      //     // action.addvalue - изменить на прилетевшее
+      //   ]
+      // };
       console.log('state после обработки редьюсером:',newState);
       return newState;
     }
