2797942681b2fca38c83e161d739061ccaa8e088
AlexandraHessen <xl_energy@mail.ru>
2021-12-16 20:17:05 +0300

    orderAC

--- redux/orderAC.js
+++ redux/orderAC.js
@@ -6,10 +6,11 @@ const ADD_ORDER = "ADD_ORDER"
 //функция для dispatch (из файла CounterButton) которая возвращает хэш с action type 
 // называется ActionCreator
 
-const add_order = function(objOrderInfo) {
+const add_order = function(customerPhone, objOrderInfo) {
 // const add_product = function(productId, addValue) {
     return {
       type: ADD_ORDER,
+      customerPhone: customerPhone,
       objOrderInfo: objOrderInfo
     };
   }
