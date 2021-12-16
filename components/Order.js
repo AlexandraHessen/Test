9e0a8f2314bcc0f8596c4c3692735c07e493ef6e
AlexandraHessen <xl_energy@mail.ru>
2021-12-16 21:55:28 +0300

    valid all

--- components/Order.js
+++ components/Order.js
@@ -15,6 +15,9 @@ class Order extends React.PureComponent{
         surname: '',
         tel: '',
         email: '',
+        delivery: 'pickup',
+        payment: 'cash',
+        note: '',
 
         nameNotValid: false,    // когда ошибка = true отображается  <span> с ошибкой  (логич выражение) && JSX
         surnameNotValid: false,
@@ -27,124 +30,220 @@ class Order extends React.PureComponent{
         surnameError: 'Поле "Фамилия" обязательно для заполнения!',
         telError: 'Поле "Телефон" обязательно для заполнения. Введите корректный телефон!',
         emailError: 'Поле "E-mail" обязательно для заполнения. Введите корректный E-mail!',
+
+        isOrderMade: false
     }
 
     changeInput = (EO) => {
-        this.setState({[EO.target.name]: EO.target.value,
-            })
+        this.setState({[EO.target.name]: EO.target.value})
             // this.props.cbChanged(true)
     }
 
-    validName = (EO) => {
+    validate = (EO) =>{
+        // ----------------------- ВАЛИДАЦИЯ ВСЕХ ПОЛЕЙ ПРИ УХОДЕ С 1 ПОЛЯ-----------------------//
         if (this.state.name === ""){
-            this.setState({nameNotValid: true})
-            // this.setState({nameNotValid: true}, this.validAll)
-            return false
+            // this.setState({nameNotValid: true})
+            this.setState({nameNotValid: true}, this.validAll)
         } else{
-            this.setState({nameNotValid: false})
-            // this.setState({nameNotValid: false}, this.validAll)
-            return true
+            // this.setState({nameNotValid: false})
+            this.setState({nameNotValid: false}, this.validAll)
+    
         }
-    }
-    validSurname = (EO) => {
+        
         if (this.state.surname === ""){
             this.setState({surnameNotValid: true}, this.validAll)
         } else{
             this.setState({surnameNotValid: false}, this.validAll)
+          
         }
-    }
-    validTel = (EO) => {
-        let tel = this.state.tel
-        const reg = /^(\+375|80)\s?-?\(?(29|25|44|33|17)\s?-?\)?(\d{3})\s?-?(\d{2})\s?-?(\d{2})$/;
-        if (tel === "" || reg.test(tel)){
+        
+        // let tel = this.state.tel
+        // const reg = /^(\+375|80)\s?-?\(?(29|25|44|33|17)\s?-?\)?(\d{3})\s?-?(\d{2})\s?-?(\d{2})$/;
+        if (this.state.tel === "" || !(/^\+375|80(\s+)?\(?(29|33|44|25|17)\)?(\s+)?[0-9]{3}-[0-9]{2}-[0-9]{2}$/.test(this.state.tel))){
             this.setState({telNotValid: true}, this.validAll)
         } else{
             this.setState({telNotValid: false}, this.validAll)
+           
         }
-    }
-    validEmail= (EO) => {
-        let email = this.state.email
-        const reg = /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/;
-        if (this.state.email === "" || reg.test(email) ){
+        
+        // let email = this.state.email
+        // const reg = /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/;
+        // if (this.state.email === "" || reg.test(email) ){
+        if (this.state.email === "" || !(/^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(this.state.email))){
             this.setState({emailNotValid: true}, this.validAll)
         } else{
             this.setState({emailNotValid: false}, this.validAll)
+        
         }
-    }
 
-    validAll=()=>{
-   
-        this.validName()
+            }
+    
+
+    // validName = (EO) => {
+    //     if (this.state.name === ""){
+    //         // this.setState({nameNotValid: true})
+    //         this.setState({nameNotValid: true}, this.validAll)
+    //     } else{
+    //         // this.setState({nameNotValid: false})
+    //         this.setState({nameNotValid: false}, this.validAll)
+    //         return true
+    //     }
+    // }
+    // validSurname = (EO) => {
+    //     if (this.state.surname === ""){
+    //         this.setState({surnameNotValid: true}, this.validAll)
+    //     } else{
+    //         this.setState({surnameNotValid: false}, this.validAll)
+    //         return true
+    //     }
+    // }
+    // validTel = (EO) => {
+    //     let tel = this.state.tel
+    //     const reg = /^(\+375|80)\s?-?\(?(29|25|44|33|17)\s?-?\)?(\d{3})\s?-?(\d{2})\s?-?(\d{2})$/;
+    //     if (tel === "" || reg.test(tel)){
+    //         this.setState({telNotValid: true}, this.validAll)
+    //     } else{
+    //         this.setState({telNotValid: false}, this.validAll)
+    //         return true
+    //     }
+    // }
+    // validEmail= (EO) => {
+    //     let email = this.state.email
+    //     // const reg = /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/;
+    //     // if (this.state.email === "" || reg.test(email) ){
+    //     if (this.state.email === ""){
+    //         this.setState({emailNotValid: true}, this.validAll)
+    //     } else{
+    //         this.setState({emailNotValid: false}, this.validAll)
+    //         return true
+    //     }
+    // }
+
+    validAll=(EO)=>{
+        // console.log(EO.target.value)
         if (this.state.nameNotValid||
             this.state.surnameNotValid||
             this.state.telNotValid||
             this.state.emailNotValid
             )
-                {this.setState({notValidForm: true})
-        } else {
+                {this.setState({notValidForm: true}) //button disabled=true
+        } else  {
             this.setState({notValidForm: false})
     }
+        
+        
+    //     {
+    //         if(this.state.name!==""){
+    //             {this.setState({notValidForm: false})
+    //                 if(EO.target="button"){
+    //                     this.props.dispatch( add_order({
+    //                 // code: this.props.code,
+    //                 name: this.state.name,
+    //                 surname: this.state.surname,
+    //                 tel: this.state.tel,
+    //                 email: this.state.email,
+    //             }) )
+    //             }
+
+    //         }
+    //     }
+            
+    // }
+    // console.log(this.state.nameNotValid)
     }
 
 
     addOrder=()=>{
-   
+        // let validName=this.validName();
+        // let validSurname = this.validSurname();
+        // let validTel =this.validTel();
+        // let validEmail =this.validEmail();
+        // if(validName && validSurname && validTel && validEmail){
+            // let validName=this.validName();
+            // let validSurname = this.validSurname();
+            // let validTel =this.validTel();
+            // let validEmail =this.validEmail();
+            // if(this.validName() && this.validSurname() && this.validTel() && this.validEmail()){
+            this.setState({isOrderMade: true})
+            this.props.dispatch( add_order( this.state.tel,
+                        {
+                            name: this.state.name,
+                            surname: this.state.surname,
+                            tel: this.state.tel,
+                            email: this.state.email,
+                            delivery: this.state.delivery,
+                            payment: this.state.payment,
+                            note: this.state.note,
+                        }) )
+        // }
+        // console.log(this.state.notValidForm)
+        // if(  this.validName()&&this.validSurname()&&this.validTel()&&this.validEmail()){
+        // this.props.dispatch( add_order({
+        //     // code: this.props.code,
+        //     name: this.state.name,
+        //     surname: this.state.surname,
+        //     tel: this.state.tel,
+        //     email: this.state.email,
+        // }) )
+        // }
 
-        this.props.dispatch( add_order({
-            // code: this.props.code,
-            name: this.state.name,
-            surname: this.state.surname,
-            tel: this.state.tel,
-            email: this.state.email,
-        }) )
+        // this.props.dispatch( add_order({
+        //     // code: this.props.code,
+        //     name: this.state.name,
+        //     surname: this.state.surname,
+        //     tel: this.state.tel,
+        //     email: this.state.email,
+        // }) )
       }
 
     render(){
         return(
-            <div className="Order">
+            (this.state.isOrderMade)?
+            (<div className="OrderMade"><h1>Ваш заказ оформлен!</h1></div>):
+            (<div className="Order">
                 <h1>Оформление заказа:</h1>
                 <div>
                     <label htmlFor="name" className="LabelOrder">Имя</label>
-                    <input type="text" name="name" className="InputOrder" onChange={this.changeInput} onBlur={this.validName}></input>
+                    <input type="text" name="name" className="InputOrder" onChange={this.changeInput} onBlur={this.validate} autoFocus></input>
                     {(this.state.nameNotValid)&&<span className="Error">{this.state.nameError}</span>} 
                 </div>
                 <div>
                     <label htmlFor="surname" className="LabelOrder">Фамилия</label>
-                    <input type="text" name="surname" className="InputOrder" onChange={this.changeInput} onBlur={this.validSurname}></input>
+                    <input type="text" name="surname" className="InputOrder" onChange={this.changeInput} onBlur={this.validate}></input>
                     {(this.state.surnameNotValid)&&<span className="Error">{this.state.surnameError}</span>} 
                 </div>
                 <div>
                     <label htmlFor="tel" className="LabelOrder">Телефон</label>
-                    <input type="text" name="tel" className="InputOrder" onChange={this.changeInput} onBlur={this.validTel}></input>
+                    <input type="text" name="tel" className="InputOrder" placeholder="+37529111-22-33" onChange={this.changeInput} onBlur={this.validate}></input>
                     {(this.state.telNotValid)&&<span className="Error">{this.state.telError}</span>} 
                 </div>
                 <div>
-                    <label htmlFor="email" className="LabelOrder" onChange={this.changeInput} onBlur={this.validEmail}>Email</label>
-                    <input type="text" name="email" className="InputOrder"></input>
+                    <label htmlFor="email" className="LabelOrder">Email</label>
+                    <input type="text" name="email" className="InputOrder" placeholder="email@gmail.com"onChange={this.changeInput} onBlur={this.validate}></input>
                     {(this.state.emailNotValid)&&<span className="Error">{this.state.emailError}</span>} 
                 </div>
                 <div>
                     <label htmlFor="delivery" className="LabelOrder">Способ доставки</label>
-                    <select name="delivery" className="InputOrder">
-                        <option value = {0}>Самовывоз</option>
-                        <option value = {1}>Почтой</option>
-                        <option value = {2}>Курьером по Минску</option>
+                    <select name="delivery" className="InputOrder" onChange = {this.changeInput}>
+                        <option value = {"pickup"}>Самовывоз</option>
+                        <option value = {"post"}>Почтой</option>
+                        <option value = {"courier"}>Курьером по Минску</option>
                     </select>
                 </div>
                 <div>
                     <label htmlFor="payment" className="LabelOrder">Способ оплаты</label>
-                    <select name="payment" className="InputOrder">
-                        <option value = {0}>Наличными</option>
-                        <option value = {1}>Пластиковой картой</option>
-                        <option value = {2}>ЕРИП</option>
+                    <select name="payment" className="InputOrder" onChange = {this.changeInput}>
+                        <option value = {"cash"}>Наличными</option>
+                        <option value = {"card"}>Пластиковой картой</option>
+                        <option value = {"erip"}>ЕРИП</option>
                     </select>
                 </div>
                 <div>
                     <label htmlFor="note" className="LabelOrder">Примечание</label>
-                    <textarea name="note" className="TextareaOrder"></textarea>
+                    <textarea name="note" className="TextareaOrder" onChange = {this.changeInput}></textarea>
                 </div>
                 <input type="button" value="Оформить заказ" className="CheckoutButton" onClick = {this.addOrder} disabled={this.state.notValidForm}></input>
-            </div>
+            </div>)
         )
     }
 }
