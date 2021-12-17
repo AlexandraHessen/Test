87546b5f24d567df11024d4d61267f144a2ed3bb
AlexandraHessen <xl_energy@mail.ru>
2021-12-17 23:52:50 +0300

    Reducer set_localStorage

--- redux/basketReducer.js
+++ redux/basketReducer.js
@@ -1,6 +1,6 @@
 // ---------------- САМА ФУНКЦИЯ REDUСER  ----------------//
 // REDUCER не делает запрос в Ajax, логика должна быть в компоненте
-import { ADD_PRODUCT, DEL_PRODUCT, CLEAR_BASKET} from './basketAC'; //название action type
+import { ADD_PRODUCT, DEL_PRODUCT, CLEAR_BASKET, SET_LOCAL_STORAGE} from './basketAC'; //название action type
 
 
 const initState={ // начальный state
@@ -41,6 +41,22 @@ function basketReducer(state=initState,action) {
     case CLEAR_BASKET:{
       let newState = {...state};
       newState.productsInBasket={};
+      return newState;
+    }
+
+    case SET_LOCAL_STORAGE: {
+      console.log('action:',action);
+      console.log('state до обработки редьюсером:',state);
+      let newState={...state, //старый state
+        productsInBasket:{...state.productsInBasket, //заменить старые продукты на теже +
+          // ПРИЛЕТИТ какой продукт (productid) изменить и насколько(addvalue)
+          [action.productId]:action.objProductInfo //+
+          //[action.counterid] - В нужном продукте 
+          // state.productsInBasket[action.productid] - его старое значение 
+          // action.addvalue - изменить на прилетевшее
+        }
+      };
+      console.log('state после обработки редьюсером:',newState);
       return newState;
     }
 
