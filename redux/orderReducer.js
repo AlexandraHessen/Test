826545e96f6acd6529a03f490e467a531af93c77
AlexandraHessen <xl_energy@mail.ru>
2021-12-20 23:55:06 +0300

    order Reducer arr

--- redux/orderReducer.js
+++ redux/orderReducer.js
@@ -4,7 +4,7 @@ import { ADD_ORDER } from './orderAC'; //название action type
 
 
 const initState={ // начальный state
-    orders: {}
+    orders: []
     //КЛЮЧИ productId, objProductInfo
     // 1: {code: 1, name: 'Замиокулькас'}
 
@@ -12,75 +12,67 @@ const initState={ // начальный state
 //   data: null, //даные которые мы загрузили по сети
 
 }
+//allInfoOrder-
+// allInfoOrder.push( { objCustomerInfo: objCustomerInfo , products: this.props.basket.productsInBasket } );
 
 function orderReducer(state=initState,action) {
   switch (action.type) {
-
     case ADD_ORDER: {
       console.log('action:',action);
-      console.log(action.customerPhone)
+      console.log(action.objCustomerInfo)
       console.log(action.objOrderInfo)
       console.log('state до обработки редьюсером:',state);
-      let newState={...state, //старый state
-        orders:{...state.orders, //заменить старые продукты на теже +
-          // ПРИЛЕТИТ какой продукт (productid) изменить и насколько(addvalue)
-          
-          [action.customerPhone]:action.objOrderInfo //+
-          //[action.counterid] - В нужном продукте 
-          // state.productsInBasket[action.productid] - его старое значение 
-          // action.addvalue - изменить на прилетевшее
-        }
-      };
-      console.log('state после обработки редьюсером:',newState);
-      return newState;
-    
-      
+      let newState={...state}
+      console.log(newState.orders)
+      newState.orders.push( { objCustomerInfo: action.objCustomerInfo, products: action.objOrderInfo } );
       // let newState={...state, //старый state
-      //   orders=[ ...state.orders,//заменить старые заказы на теже +
-      //   // orders:[...state.orders, //заменить старые продукты на теже +
+      //   // { objCustomerInfo: objCustomerInfo , products: this.props.basket.productsInBasket }
+      //   orders:{...state.orders, //заменить старые продукты на теже +
       //     // ПРИЛЕТИТ какой продукт (productid) изменить и насколько(addvalue)
-      //     state.orders[2].push(action.objProductInfo) //+
+      //     objCustomerInfo: action.objCustomerInfo, 
+      //     products: action.objOrderInfo
+          
+      //     // [action.customerPhone]:action.objOrderInfo //+
       //     //[action.counterid] - В нужном продукте 
       //     // state.productsInBasket[action.productid] - его старое значение 
       //     // action.addvalue - изменить на прилетевшее
-      //   ]
+      //   }
       // };
       console.log('state после обработки редьюсером:',newState);
       return newState;
+    
+      // console.log('state после обработки редьюсером:',newState);
+      // return newState;
     }
 
-    // case DEL_PRODUCT: {
-    //   let newState = {...state};
-    //   delete newState.productsInBasket[action.productId];
-    //   return newState;
-    // }
-
-    // case CLEAR_BASKET:{
-    //   let newState = {...state};
-    //   newState.productsInBasket={};
-    //   return newState;
-    // }
-
-    // case PLANTS_ERROR: {
-    //   let newState={
-    //     status:2,
-    //     data:null,
+    // case ADD_ORDER: {
+    //   console.log('action:',action);
+    //   console.log(action.objCustomerInfo)
+    //   console.log(action.objOrderInfo)
+    //   console.log('state до обработки редьюсером:',state);
+    //   let newState={...state, //старый state
+    //     orders:{...state.orders, //заменить старые продукты на теже +
+    //       // ПРИЛЕТИТ какой продукт (productid) изменить и насколько(addvalue)
+          
+    //       [action.customerPhone]:action.objOrderInfo //+
+    //       //[action.counterid] - В нужном продукте 
+    //       // state.productsInBasket[action.productid] - его старое значение 
+    //       // action.addvalue - изменить на прилетевшее
+    //     }
     //   };
+    //   console.log('state после обработки редьюсером:',newState);
     //   return newState;
-    // }
-
-    // case PLANTS_SET: {
-    //   let newState={
-    //     status:3,
-    //     data:action.plants, //загруженные данные
-    //   };
-    //   return newState;
-    // }
     
+    //   console.log('state после обработки редьюсером:',newState);
+    //   return newState;
+    // }
+
+
 //обязатнльно пишем на тот случай если пришел action который не относится к этому reducery
     default:
       return state;
   }
 }
+
 
 export default orderReducer;
