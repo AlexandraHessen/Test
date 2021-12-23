da51a06fa25158c9bf8db0508b432733bf5a0cd7
AlexandraHessen <xl_energy@mail.ru>
2021-12-23 17:38:37 +0300

    BasketPage add isNeedToWarn

--- pages/BasketPage.js
+++ pages/BasketPage.js
@@ -4,6 +4,7 @@ import {connect} from 'react-redux';
 
 import { ADD_PRODUCT, DEL_PRODUCT } from '../redux/basketAC';
 import {plantsEvents} from '../components/events';
+// import {withRouter} from 'react-router';
 
 import Basket from '../components/Basket'
 import "./BasketPage.css"
@@ -11,7 +12,8 @@ import "./BasketPage.css"
 class BasketPage extends React.Component{
 
     state={
-        isMadeOrder: false
+        isMadeOrder: false,
+        isNeedToWarn: false,
     }
     static propTypes = {
         basket: PropTypes.object.isRequired, // пришло из Redux для проверки есть ли что-то в корзине
@@ -19,15 +21,33 @@ class BasketPage extends React.Component{
     }
     componentDidMount =()=>{
         plantsEvents.addListener('EvMadeOrder', this.evMadeOrder);
+        plantsEvents.addListener('EvNeedToWarn', this.evNeedToWarn);
         this.scrollTop();
+        // console.log(this.state.isNeedToWarn)
+        // if(this.state.isNeedToWarn){
+            
+        //     window.onbeforeunload = function() {
+        //         return true;
+        //     };
+        // }
+  
     }
 
     componentWillUnmount =()=>{
         plantsEvents.removeListener('EvMadeOrder', this.evMadeOrder);
+        plantsEvents.removeListener('EvNeedToWarn', this.evNeedToWarn);
+        window.onbeforeunload = null;
     }
+     // componentDidUnmount(){
+    //     window.onbeforeunload = null;
+    // }
 
     evMadeOrder=()=>{
         this.setState({isMadeOrder: true})
+    }
+
+    evNeedToWarn=()=>{
+        this.setState({isNeedToWarn: true})
     }
 
     scrollTop = () => {
@@ -37,7 +57,21 @@ class BasketPage extends React.Component{
 
 
     render(){
-        this.scrollTop();
+        // <Route path="/favorites" render={() =>
+        //     <div>То что нужно показать если url == /favorites*</div>}>
+        // console.log(this.props.location.pathname)
+        // console.log(this.props.location.pathname)
+        // console.log((this.props.location.pathname === "/basket" )&& this.state.isNeedToWarn)
+        
+        // if((this.props.location.pathname == "/basket" )&& this.state.isNeedToWarn){
+        if(this.state.isNeedToWarn){
+            window.onbeforeunload = function() {
+                return true;
+            };
+        }
+        // console.log(this.state.isNeedToWarn)
+        // console.log(this.props.history.location.pathname)
+        // this.scrollTop();
         //????????????? а сели другие ключи будут 
         let countProductsInBasket=Object.keys(this.props.basket.productsInBasket).length;
         return(
@@ -49,7 +83,7 @@ class BasketPage extends React.Component{
                     (<div><h1 className="OrderMade">Ваш заказ оформлен!</h1></div>) :
                     (<h3>Корзина пуста.</h3>) ) : 
 
-                <Basket />}
+                <Basket/>}
             </div>
         )
     }
@@ -64,5 +98,6 @@ const mapStateToProps = function (state) {
   };
   
   export default connect(mapStateToProps)(BasketPage);
+//   export default withRouter(connect(mapStateToProps)(BasketPage));
   // экспортируем не этот класс CounterButton а уже обвернутый
   
