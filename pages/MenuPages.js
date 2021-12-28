f6c1aff35ddadae77608a907f29a9d8c7e6a13b8
AlexandraHessen <xl_energy@mail.ru>
2021-12-28 22:14:03 +0300

    MenuPages

--- pages/MenuPages.js
+++ pages/MenuPages.js
@@ -8,7 +8,6 @@ class MenuPages extends React.Component{
       // нельзя pure componenet
     render(){
         return(
-            // <div className="MenuPages">
             <ul className="MenuPages">
                 <li><NavLink to="/" exact className="PageLink" activeClassName="ActivePageLink">Главная</NavLink></li>
                 <li><NavLink to="/catalog/1" className="PageLink" activeClassName="ActivePageLink">Каталог</NavLink></li>
